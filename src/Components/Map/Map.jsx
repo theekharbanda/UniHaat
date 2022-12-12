6247c348555d1f4bb1d0238b42bdfa9cf440a818
Naman Kharbanda <namankharbanda512@gmail.com>
2022-12-13 05:29:11 +0530

    Initial Commit

---
+++ src/Components/Map/Map.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+
+export default function Map(){
+    return(
+        <div className="map">
+            <Box sx={{
+                
+            }}>
+
+            </Box>
+        </div>
+    );
+}
